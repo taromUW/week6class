1997e57d0ee9e3c8d43a8eee40d6e7e9fed38792
Taro Miwa <tmiwa@wwu.edu>
2024-06-04 20:31:01 -0700

    clean up

--- daos/note.js
+++ daos/note.js
@@ -1,20 +1,5 @@
-/*
-- createNote(userId, noteObj) - should create a note for the given user
-- getNote(userId, noteId) - should get note for userId and noteId (\_id)
-- getUserNotes(userId) - should get all notes for userId 
-*/
 const Note = require('../models/note');
 module.exports = {};
-
-/* module.exports.createNote = (userId, noteObj) => {
-  return Note.create({userId:userId, text:noteObj});
-}
- */
-
-/* module.exports.createNote = (userId, noteObj, categoryId) => {
-  return Note.create({userId:userId, text:noteObj, categoryId: categoryId});
-}
- */
 
 module.exports.createNote = (noteData) => {
     return Note.create(noteData);
@@ -46,7 +31,6 @@ module.exports.removeById = async (noteId) => {
 
 module.exports.getStats = () => {
   return Note.aggregate([
-//    { $match: { userId }},
     { $group: { _id: '$userId', count: { $sum: 1},},},
   ]);
 };
--- daos/token.js
+++ daos/token.js
@@ -1,8 +1,3 @@
-/*
-- makeTokenForUserId(userId) - should be an async function that returns a string after creating a Token record
-- getUserIdFromToken(tokenString) - should be an async function that returns a userId string using the tokenString to get a Token record
-- removeToken(tokenString) - an async function that deletes the corresponding Token record
-*/
 const Token = require('../models/token');
 const uuid = require('uuid');
 module.exports = {};
